7f449605dfe307ff56ab1ad09bd22f4d7481d27c
0o0slytherinpride0o0 <hillary.animenerd@gmail.com>
2025-09-09 12:10:18 -0400

    Create Neopets - User Shops Item Autofocus.user.js

---
+++ Neopets - User Shops Item Autofocus.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           Neopets - User Shops Item Autofocus
+// @version        1.0
+// @description	   Autofocuses the item you want to buy, scrolling it into view & making it so you just have to hit Enter (twice) to purchase!
+//                 (Set focusVisible to true if you want the link to be highlighted)
+// @author         0o0slytherinpride0o0
+// @match          *://*.neopets.com/browseshop.phtml?owner=*
+// ==/UserScript==
+
+document.addEventListener("DOMContentLoaded", () => {
+  var link = document.querySelector('div:has(+ hr[size="1"][width="75%"]) a[onclick]');
+  if (link) {
+    link.setAttribute("taxindex","-1");
+    link.focus({ focusVisible: false }); // set to true if you want the link highlighted
+  }
+}, { once: true });

68628e1402b42fb4aba28c9c26b3ef765bd71315
0o0slytherinpride0o0 <hillary.animenerd@gmail.com>
2025-09-11 11:12:52 -0400

    Update Neopets - User Shop Scroll Item into View.user.js
    Made compatible with Tampermonkey.
    - removed the :has() selector, which Tampermonkey doesn't support
    - switched to window.onload instead of DOMContentLoaded, because Tampermonkey already waits until after DOMContentLoaded
    ^ TIL for both of these - thanks ChatGPT!

--- Neopets - User Shop Scroll Item into View.user.js
+++ Neopets - User Shop Scroll Item into View.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Neopets - User Shop Scroll Item into View
-// @version        1.1
+// @version        1.2
 // @description	   Scrolls the item you want to purchase into view (after clicking on a SSW/SW link)
 //                 This only really matters for shops with a lot of banners/images/text/etc. Otherwise there will be no (visible) effect.
 //                 Note that using a script to remove user code from shops has the same advantage of ensuring the item you wish to purchase is in view.
@@ -8,11 +8,17 @@
 // @match          *://www.neopets.com/browseshop.phtml?owner=*
 // ==/UserScript==
 
-document.addEventListener("DOMContentLoaded", () => {
-  var link = document.querySelector('div:has(+ hr[size="1"][width="75%"]) a[onclick]');
-  if (link) {
-    link.setAttribute("tabindex","-1");
-    link.focus({ focusVisible: false }); // set to true if you want the link highlighted
-    link.blur(); // removes the focus because I was told this was too much of an advantage
-  }
-}, { once: true });
+(function() {
+    'use strict';
+    window.onload = function() { 
+        var hr = document.querySelector('hr[size="1"][width="75%"]'); 
+        if (hr) {
+            var link = hr.previousElementSibling.querySelector("a[onclick]");
+            if (link) {
+                link.setAttribute("tabindex","-1");
+                link.focus({ focusVisible: false }); 
+                link.blur(); // unfocuses the item after it's scrolled into view
+            }
+        }
+    }
+})();
